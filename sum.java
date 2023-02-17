6d3fac433e77dd4175394375b3a9fb207ffc716e
neha2kk3 <125586817+neha2kk3@users.noreply.github.com>
2023-02-17 21:04:24 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,14 @@
+public class sum
+{
+ public static void main(String args[])
+{
+  int a,b,c,d,e,sum;
+  a=2;
+  b=5;
+  c=1;
+  d=3;
+  e=9;
+  sum=a+b+c+d+e;
+  System.out.println("sum of 5 numbers is:"+sum);
+}
+}
